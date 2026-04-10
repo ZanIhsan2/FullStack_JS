97c74b111197b76937d6c174bdc4e131a189247d
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-04-10 14:10:25 +0700

    feat: add routing koneksi db di app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,28 @@
 const express = require('express'); //import express
 const router = require('./routes/api');
+const db = require('./config/database'); //import database
+
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded)
-
+app.use(express.urlencoded());
 app.use(router);
 
-// daftarkan port
+// Tambahkan ENDPOINT API untuk test database
+app.get('/test-db', async (req,res) => {
+    db.query('SELECT 1', (err, results) => {
+        if (err) {
+            res.json({ message: "Koneksi Database Gagal" });
+        } else {
+            res.json({ 
+                message: "Koneksi Database Berhasil",
+            results: results 
+            });
+        }
+    });
+});
+
+// pindahkan routing ke routes/api.js
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
